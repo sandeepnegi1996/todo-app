1f410668cd6bcdcfe6846657ef9716b6d11afc71
sandeepnegi1996 <9041128179sandy@gmail.com>
2019-08-24 01:32:54 +0530

    finally completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'todo-app';
+  todoArray = [];
+  addTodo(value) {
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+  }
+
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
 }
